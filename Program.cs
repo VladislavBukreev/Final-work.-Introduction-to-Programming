443f346905ff95f08499e0b07f4c974765745ecd
VladislavBukreev <bukr7@mail.ru>
2023-03-26 23:07:16 +1000

    Readme + Opisanie KODA

--- Program.cs
+++ Program.cs
@@ -2,3 +2,44 @@
 //Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 //При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+Console.Clear();
+Console.WriteLine("Ведите любые слова через пробел :");
+string stroka = Console.ReadLine()!;
+string[] array = stroka.Split(" ");
+int size = SortingMethod(array);
+Console.WriteLine($" [    {String.Join(", ", NewArray(size))}    ]");
+
+
+int SortingMethod(string[] array)
+{
+    int res = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            res = res + 1;
+        }
+    }
+    return res;
+}
+
+
+string[] NewArray(int size)
+{
+    string[] ArrayRes = new string[size];
+    int i = 0;
+    int j = 0;
+    while (i < array.Length)
+    {
+        if (array[i].Length <= 3)
+        {
+            ArrayRes[j] = array[i];
+            j = j + 1;
+        }
+        i = i + 1;
+    }
+    return ArrayRes;
+
+}
+
+Console.ReadLine();
